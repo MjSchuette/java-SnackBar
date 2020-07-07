35f9a933047ffaf61527980b479395b6f8f6ed23
MatthewSchuette <mjschuette4@gmail.com>
2020-07-07 11:46:59 -0500

    Set up snack.java, customer.java, and vendingmachine.java

--- src/snackBar/VendingMachine.java
+++ src/snackBar/VendingMachine.java
@@ -2,4 +2,28 @@ package snackBar;
 
 public class VendingMachine {
     
+    private static int maxId = 0;
+    private int id;
+    private String name;
+
+    public VendingMachine(String name){
+        maxId++;
+        
+        id = maxId;
+        this.name = name;
+    }
+
+    //Getter functions
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //Setter functions
+    public void setName(String name) {
+        this.name = name;
+    }
 }
